7547a6a3900fa6b6a73cc9f1853a771e60726e2f
Eduardo Tomasi <eduardo.tomasi.1994@gmail.com>
2019-11-18 17:58:36 -0300

    Exemplo do Decoration Pattern.

---
+++ DesignPatternsParteUm/PatternDecorator-ImpostosCompostos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternDecorator_ImpostosCompostos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Imposto iss = new ISS(new ICMS()); = 80
+            Imposto iss = new ISS();
+
+            Orcamento orcamento = new Orcamento(500);
+
+            double valor = iss.Calcula(orcamento);
+
+            Console.WriteLine(valor);
+
+            Console.ReadKey();
+        }
+    }
+}
