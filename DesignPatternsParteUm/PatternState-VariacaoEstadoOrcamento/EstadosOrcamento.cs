8b1b2cce029d072d3003e0dc940a843c43f0e7c5
Eduardo Tomasi <eduardo.tomasi.1994@gmail.com>
2019-11-19 17:53:06 -0300

    Adicionado o Pattern State.

---
+++ DesignPatternsParteUm/PatternState-VariacaoEstadoOrcamento/EstadosOrcamento.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace PatternState_VariacaoEstadoOrcamento
+{
+    public class EmAprovacao : IEstadoDeUmOrcamento
+    {
+        public void AplicaDescontoExtra(Orcamento orcamento) => orcamento.Valor -= orcamento.Valor * 0.05;
+
+        public void Aprova(Orcamento orcamento)
+        {
+            // do estado atual "em aprovacao" eu posso aprovar o orçamento.
+            orcamento.Estado = new Aprovado();
+        }
+
+        public void Finaliza(Orcamento orcamento)
+        {
+            // do estado atual "em aprovacao" eu não posso finalizar o orçamento.
+            throw new Exception("Orçamento em aprovação não pode ir para finalizado.");
+        }
+
+        public void Reprova(Orcamento orcamento)
+        {
+            // do estado atual "em aprovacao" eu posso reprovar o orçamento.
+            orcamento.Estado = new Reprovado();
+        }
+    }
+
+    public class Aprovado : IEstadoDeUmOrcamento
+    {
+        public void AplicaDescontoExtra(Orcamento orcamento) => orcamento.Valor -= orcamento.Valor * 0.02;
+
+        public void Aprova(Orcamento orcamento)
+        {
+            // do estado atual "aprovado" eu não posso aprovar o orçamento.;
+            throw new Exception("Orçamento já está em estado de aprovado.");
+        }
+
+        public void Finaliza(Orcamento orcamento)
+        {
+            // do estado atual "aprovado" eu posso finalizar o orçamento.
+            orcamento.Estado = new Finalizado();
+        }
+
+        public void Reprova(Orcamento orcamento)
+        {
+            // do estado atual "aprovado" eu não posso reprovar o orçamento.
+            throw new Exception("Orçamento está em estado de aprovação e não pode ser reprovado.");
+        }
+    }
+
+    public class Reprovado : IEstadoDeUmOrcamento
+    {
+        public void AplicaDescontoExtra(Orcamento orcamento) => throw new Exception("Orçamentos reprovados não recebem desconto extra.");
+
+        public void Aprova(Orcamento orcamento)
+        {
+            throw new Exception("Orçamentos reprovados não podem ser aprovados.");
+        }
+
+        public void Finaliza(Orcamento orcamento)
+        {
+            throw new Exception("Orçamentos reprovados não podem ser finalizados.");
+        }
+
+        public void Reprova(Orcamento orcamento)
+        {
+            throw new Exception("Orçamentos reprovados não podem ser reprovados.");
+        }
+    }
+
+    public class Finalizado : IEstadoDeUmOrcamento
+    {
+        public void AplicaDescontoExtra(Orcamento orcamento) => throw new Exception("Orçamentos finalizados não recebem desconto extra.");
+
+        public void Aprova(Orcamento orcamento)
+        {
+            throw new Exception("Orçamentos finalizados não podem ser aprovados.");
+        }
+
+        public void Finaliza(Orcamento orcamento)
+        {
+            throw new Exception("Orçamentos finalizados não podem ser finalizados.");
+        }
+
+        public void Reprova(Orcamento orcamento)
+        {
+            throw new Exception("Orçamentos finalizados não podem ser reprovados.");
+        }
+    }
+}
---
+++ DesignPatternsParteUm/PatternState-VariacaoEstadoOrcamento/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternState_VariacaoEstadoOrcamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Orcamento reforma = new Orcamento(500);
+
+            Console.WriteLine(reforma.Valor);
+
+            reforma.AplicaDescontoExtra();
+            Console.WriteLine(reforma.Valor);
+            reforma.Aprova();
+
+            reforma.AplicaDescontoExtra();
+            Console.WriteLine(reforma.Valor);
+
+            reforma.Finaliza();
+
+            //reforma.AplicaDescontoExtra();
+            reforma.Aprova();
+            
+        }
+    }
+}
