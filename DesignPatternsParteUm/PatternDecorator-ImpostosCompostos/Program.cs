7538a02b4377335cb4811c6cf2a317893562ac73
Eduardo Tomasi <eduardo.tomasi.1994@gmail.com>
2019-11-19 12:56:54 -0300

    Criado exemplo de uso do Decorator em conjunto com o Template Method.

--- DesignPatternsParteUm/PatternDecorator-ImpostosCompostos/Program.cs
+++ DesignPatternsParteUm/PatternDecorator-ImpostosCompostos/Program.cs
@@ -10,14 +10,34 @@ namespace PatternDecorator_ImpostosCompostos
     {
         static void Main(string[] args)
         {
-            //Imposto iss = new ISS(new ICMS()); = 80
-            Imposto iss = new ISS();
+            #region Exemplo 1 - Decorator
+            ////Imposto iss = new ISS(new ICMS()); = 80
+            //Imposto iss = new ISS();
 
-            Orcamento orcamento = new Orcamento(500);
+            //Orcamento orcamento = new Orcamento(500);
 
-            double valor = iss.Calcula(orcamento);
+            //double valor = iss.Calcula(orcamento);
 
-            Console.WriteLine(valor);
+            //Console.WriteLine(valor);
+            #endregion
+
+            #region Exemplo 2 - Decorator
+            //Orcamento orcamento = new Orcamento(1000);
+            //Imposto impostoAlto = new ImpostoMuitoAlto(new ISS(new ICMS()));
+            //double valorTotalImposto = impostoAlto.Calcula(orcamento);
+
+            //Console.WriteLine(valorTotalImposto); 
+            #endregion
+
+            #region Exemplo 3 - Decorator e Template Method
+            //Orcamento orcamento = new Orcamento(1000);
+            //orcamento.AdicionarItem(new Item("Xiaomi Mi A2", 1000));
+
+            //Imposto imposto = new ICMS(new ICPP(new IKCV(new ISS())));
+
+            //double valorImposto = imposto.Calcula(orcamento);
+            //Console.WriteLine(valorImposto); 
+            #endregion
 
             Console.ReadKey();
         }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BoasPraticasCSharp
 Repositório contém os conteúdos da Alura sobre Boas Práticas com C#.
+
+Implementado exemplo dos padrões:
+ * Strategy.
+ * Chain of Responsability.
+ * Template Method.
+ * Decorator.
