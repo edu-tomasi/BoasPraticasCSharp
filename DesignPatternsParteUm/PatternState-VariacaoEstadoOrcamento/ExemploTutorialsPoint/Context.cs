56815e9136b85d2a1c77ffcaff64dc5910fbc2b4
Eduardo Tomasi <eduardo.tomasi.1994@gmail.com>
2019-11-25 17:46:50 -0300

    Adicionado exemplo de implementação do State Pattern.

---
+++ DesignPatternsParteUm/PatternState-VariacaoEstadoOrcamento/ExemploTutorialsPoint/Context.cs
@@ -0,0 +1,12 @@
+﻿namespace PatternState_VariacaoEstadoOrcamento
+{
+    public class Context
+    {
+        public IState state { get; set; }
+
+        public Context()
+        {
+            state = null;
+        }
+    }
+}
---
+++ DesignPatternsParteUm/PatternState-VariacaoEstadoOrcamento/ExemploTutorialsPoint/StartState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PatternState_VariacaoEstadoOrcamento
+{
+    public class StartState : IState
+    {
+        public void doAction(Context context)
+        {
+            Console.WriteLine("Player is in start state.");
+            context.state = this;
+        }
+
+        public override string ToString()
+        {
+            return "Start State";
+        }
+    }
+}
