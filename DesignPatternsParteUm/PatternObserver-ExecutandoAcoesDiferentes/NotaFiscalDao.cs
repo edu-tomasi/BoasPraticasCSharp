5771dd4531cb7eb3b5c05447045a5d771d4fa4d6
Eduardo Tomasi <eduardo.tomasi.1994@gmail.com>
2019-11-26 16:30:50 -0300

    Adicionado Observer Pattern.

---
+++ DesignPatternsParteUm/PatternObserver-ExecutandoAcoesDiferentes/NotaFiscalDao.cs
@@ -0,0 +1,17 @@
+﻿using PatternBuilder_GeradorDeNotaFiscal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternObserver_ExecutandoAcoesDiferentes
+{
+    public class NotaFiscalDao : IAcaoAposGererNota
+    {
+        public void Executa(NotaFiscal nf)
+        {
+            Console.WriteLine("DB");
+        }
+    }
+}
---
+++ DesignPatternsParteUm/PatternObserver-ExecutandoAcoesDiferentes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternObserver_ExecutandoAcoesDiferentes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
