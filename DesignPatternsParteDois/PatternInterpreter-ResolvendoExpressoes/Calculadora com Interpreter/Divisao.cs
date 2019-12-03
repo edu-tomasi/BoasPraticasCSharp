cdf9e7ab4ac5121447f01be907ce057504c2867a
Eduardo Tomasi <eduardo.tomasi.1994@gmail.com>
2019-12-03 10:03:18 -0300

    Implementado o exemplo da Calculadora utilizando o Pattern Interpreter.

---
+++ DesignPatternsParteDois/PatternInterpreter-ResolvendoExpressoes/Calculadora com Interpreter/Divisao.cs
@@ -0,0 +1,21 @@
+﻿namespace PatternInterpreter_ResolvendoExpressoes
+{
+    class Divisao : IExpressao
+    {
+        private IExpressao _esquerda;
+        private IExpressao _direita;
+
+        public Divisao(IExpressao esquerda, IExpressao direita)
+        {
+            this._esquerda = esquerda;
+            this._direita = direita;
+        }
+
+        public int Avalia()
+        {
+            int valorEsquerda = _esquerda.Avalia();
+            int valorDireita = _direita.Avalia();
+            return valorEsquerda / valorDireita;
+        }
+    }
+}
---
+++ DesignPatternsParteDois/PatternInterpreter-ResolvendoExpressoes/Calculadora com Interpreter/Numero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternInterpreter_ResolvendoExpressoes
+{
+    class Numero : IExpressao
+    {
+        private int _numero;
+
+        public Numero(int numero)
+        {
+            this._numero = numero;
+        }
+
+        public int Avalia()
+        {
+            return _numero;
+        }
+    }
+}
